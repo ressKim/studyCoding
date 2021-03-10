8180b52c467f88d699c01d1b0bc1fba9d1774d3c
ressKim <sh21dlat@gmail.com>
2021-03-10 10:49:55 +0900

    좋은답변 두개 모음 copyOfRange 공부 필요

--- src/com/company/level1/Sorting.java
+++ src/com/company/level1/Sorting.java
@@ -1,9 +1,6 @@
 package com.company.level1;
 
 import java.util.ArrayList;
-
-//import java.util.Arrays;
-//import java.util.Arrays;
 import java.util.Arrays;
 import java.util.Collections;
 
@@ -14,19 +11,39 @@ public class Sorting {
         int[] array = {1, 5, 2, 6, 3, 7, 4};
         int[][] commands = {{2, 5, 3}, {4, 4, 1}, {1, 7, 3}};
 
-        int[] result = new int[commands.length];
+        int[] answer = new int[commands.length];
 
-        for (int j = 0; j < commands.length; j++) {
+        //내가 한거
+//        for (int j = 0; j < commands.length; j++) {
+//            ArrayList<Integer> temp = new ArrayList<>();
+//            int[] test = new int[commands[j][1] - commands[j][0] + 1];
+//            for (int a = commands[j][0]; a <= commands[j][1]; a++) {
+//                test[commands[j][1] - a] = array[a - 1];
+//            }
+//            Arrays.sort(test);
+//
+//            result[j] = test[commands[j][2] - 1];
+//        }
 
-            int[] test = new int[commands[j][1] - commands[j][0] + 1];
-            for (int a = commands[j][0]; a <= commands[j][1]; a++) {
-                test[commands[j][1] - a] = array[a - 1];
-            }
-            Arrays.sort(test);
+        //좋은 답변1
+        for (int i = 0; i < commands.length; i++) {
+            int[] temp = Arrays.copyOfRange(array, commands[i][0] - 1, commands[i][1]);
+            Arrays.sort(temp);
+            answer[i] = temp[commands[i][2] - 1];
+        }
+        // 좋은답변 2
+        int a = 0;
+        for (int[] info : commands) {
+            int i = info[0];
+            int j = info[1];
+            int k = info[2];
 
-            result[j] = test[commands[j][2] - 1];
+            int[] buf = Arrays.copyOfRange(array, i - 1, j);
+            Arrays.sort(buf);
+            answer[a] = buf[k - 1];
+            a++;
         }
 
-        return result;
+        return answer;
     }
 }
